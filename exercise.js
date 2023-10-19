5a989b7795d07dd380fe07f617cbf307e9ea0a4c
SamPatt <git@sampatt.com>
2023-10-19 14:11:33 -0400

    Finished Exercise 3

--- exercise.js
+++ exercise.js
@@ -1,5 +1,3 @@
-console.log('Yo!')
-
 // Exercise 1 completed
 
 // Exercise 2 Define a function, as a function expression, maxOfThree that takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
@@ -17,4 +15,13 @@ const maxOfThree = function(num1, num2, num3) {
         return largest
     }
 }
-console.log(maxOfThree(1,2,3))
+//console.log(maxOfThree(1,2,3))
+
+//Exercise 3 Define a function, as a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
+
+function isCharAVowel(char) {
+    char = char.toUpperCase()
+    return char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'
+}
+
+console.log(isCharAVowel('a'))
