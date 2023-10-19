f83fe652c0451920ba85de961e1022ad22648f8e
SamPatt <git@sampatt.com>
2023-10-19 14:19:20 -0400

    Finished Exercise 4

--- exercise.js
+++ exercise.js
@@ -17,11 +17,23 @@ const maxOfThree = function(num1, num2, num3) {
 }
 //console.log(maxOfThree(1,2,3))
 
-//Exercise 3 Define a function, as a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
+// Exercise 3 Define a function, as a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
 
 function isCharAVowel(char) {
     char = char.toUpperCase()
     return char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'
 }
 
-console.log(isCharAVowel('a'))
+//console.log(isCharAVowel('a'))
+
+// Exercise 4 Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]); would return 11
+
+const sumArray = function(arr) {
+    let sum = 0
+    arr.forEach(element => {
+        sum = sum + element
+    });
+    return sum
+}
+
+console.log(sumArray([2, 4, 5]));
